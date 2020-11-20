df84f670cd68338e8792db915bf36e52b0ec5959
hayaB98 <57070634+hayaB98@users.noreply.github.com>
2020-11-20 11:59:33 +0200

    Update app.py

--- code/app.py
+++ code/app.py
@@ -1,23 +1,47 @@
 import os
 from flask import Flask
+from functools import wraps
 from flaskext.mysql import MySQL
 import os, json, subprocess, yaml, logging
 
 app = Flask(__name__)
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
 app.config['MYSQL_DATABASE_DB'] = 'newdatabase'
 app.config['MYSQL_DATABASE_HOST'] = 'db'
 
 mysql.init_app(app)
+log_file = "./logfile.log"
+log_level = logging.DEBUG
+logging.basicConfig(level=log_level, filename=log_file, filemode="w+", format="%(asctime)-15s %(levelname)-8s %(message)s")
+logger = logging.getLogger("logger")
+
+def wrap(pre, post): 
+    def decorate(func):
+         @wraps(func)
+         def call(*args, **kwargs):
+             pre(func)
+             result = func(*args, **kwargs)
+             post(func)
+             return result
+         return call
+    return decorate
+
+def entering(func):
+   logger.debug("Entered %s", func.__name__)
+
+def exiting(func):
+   logger.debug("Exited %s", func.__name__)
 
 @app.route('/')
+@wrap(entering,exiting)
 def inp():
      return "hello world"
 
 @app.route('/cpu')
+@wrap(entering,exiting)
 def cpu():
      cursor = mysql.connect().cursor()
      cursor.execute(''' SELECT * FROM cpu_usage WHERE taken_at >= DATE_SUB(NOW(), INTERVAL 1 DAY); ''')
@@ -26,6 +50,7 @@ def cpu():
      return json.dumps(result, indent=4, default=str)
 
 @app.route('/disk')
+@wrap(entering,exiting)
 def disk():
      cursor = mysql.connect().cursor()
      cursor.execute(''' SELECT * FROM disk_usage WHERE taken_at >= DATE_SUB(NOW(), INTERVAL 1 DAY); ''')
@@ -34,6 +59,7 @@ def disk():
      return json.dumps(result, indent=4, default=str)
 
 @app.route('/memory')
+@wrap(entering,exiting)
 def mem():
      cursor = mysql.connect().cursor()
      cursor.execute(''' SELECT * FROM mem_usage WHERE taken_at >= DATE_SUB(NOW(), INTERVAL 1 DAY); ''')
@@ -42,8 +68,8 @@ def mem():
      return json.dumps(result, indent=4, default=str)
 
 @app.route('/now')
+@wrap(entering,exiting)
 def datanow():
- #    dir = os.path.dirname(os.path.realpath(__file__)
      result = subprocess.check_output(['bash', '/project/currentvalues.sh'])
      result = yaml.load(result)
      return json.dumps(result, indent=4, default=str)
